eea309e863cac9416e593015ddf84387d00e188d
JSey <joerg.seyfried@gmail.com>
2015-11-28 21:58:53 +0100

    inching towards initial release

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,4 @@
-# name: reply-by-email-TL
+# name: reply_by_email_TL
 # about: Add site setting to restrict reply-by-email to trust level x and above
 # version: 0.0.1
 # authors: Joerg Seyfried (JSey)
@@ -6,15 +6,15 @@
 
 require_dependency 'email/message_builder'
 
-enabled_site_setting :enable_reply_by_email_TL
+enabled_site_setting :reply_by_email_TL_enabled
 
 after_initialize do
   class MessageBuilder
     class << self
       def allow_reply_by_email?
         SiteSetting.reply_by_email_enabled? &&
-        # argh: need to search recipient to determine TL...
-        ( !SiteSetting.enable_reply_by_email || User.find_by(email: @to).trust_level > SiteSetting.reply_by_email_TL ) &&
+        # either we're disabled or recipients TL needs to be above or equal to the min setting
+        ( !SiteSetting.reply_by_email_enabled || User.find_by(email: @to).trust_level >= SiteSetting.reply_by_email_TL_min ) &&
         reply_by_email_address.present? &&
         @opts[:allow_reply_by_email]
       end
