bbecd21aab665bb7cb9bbd3bf29d741621b382e1
JSey <joerg.seyfried@gmail.com>
2015-11-29 19:44:59 +0100

    fixes

--- plugin.rb
+++ plugin.rb
@@ -9,15 +9,14 @@ require_dependency 'email/message_builder'
 enabled_site_setting :reply_by_email_TL_enabled
 
 after_initialize do
-  class MessageBuilder
-    class << self
-      def allow_reply_by_email?
-        SiteSetting.reply_by_email_enabled? &&
-        # either we're disabled or recipient's TL needs to be above or equal to the min setting
-        ( !SiteSetting.reply_by_email_enabled || User.find_by(email: @to).trust_level >= SiteSetting.reply_by_email_TL_min ) &&
-        reply_by_email_address.present? &&
-        @opts[:allow_reply_by_email]
-      end
+  class Email::MessageBuilder
+    def allow_reply_by_email?
+      #Rails.logger.warn "To: #{@to} with TL #{User.find_by(email: @to).trust_level}"
+      SiteSetting.reply_by_email_enabled? &&
+      # either we're disabled or recipient's TL needs to be above or equal to the min setting
+      ( !SiteSetting.reply_by_email_enabled || User.find_by(email: @to).trust_level >= SiteSetting.reply_by_email_TL_min ) &&
+      reply_by_email_address.present? &&
+      @opts[:allow_reply_by_email]
     end
   end
 end
